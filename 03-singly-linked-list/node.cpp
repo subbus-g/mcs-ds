cfc809b4c13a30c3d91e821f12879e4f0b4c11a4
subbus <subbareddyg83@gmail.com>
2022-06-07 12:19:19 +0530

    update node

--- 03-singly-linked-list/node.cpp
+++ 03-singly-linked-list/node.cpp
@@ -34,5 +34,22 @@ int main()
 	cout << node2->next->next->data << endl;
 	cout << node3->next->data << endl;
 	cout << node4->data;
+
+	// Node* head=node1;
+	// cout<<&head;
+	printf("%u", &node1);
+	cout << &node1 << endl;
+	cout << "node 1 pointer address  " << std::dec << &node1 << endl;
+	cout << "address node pointing to  " << node1 << endl;
+	cout << "address of node1->data  " << &(node1->data) << endl;
+	cout << "address of node1->next`  " << &(node1->next) << endl;
+
+	printf("address of node1->data is %u\n", &(node1->data));
+	printf("address of node1->next is %u\n", &(node1->next));
+
+	// int arr[4] = {1, 2, 3, 4};
+	// printf("%u\n", arr);
+	// printf("%u\n", &arr[0]);
+	// printf("%u\n", &arr[1]);
 	return 0;
 }
