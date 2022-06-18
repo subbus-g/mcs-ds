f235cea6a58702f013a6d707d6f370ec8648224c
subbus <subbareddyg83@gmail.com>
2022-06-18 07:27:03 +0530

    add hw-1 destructor

---
+++ 03-singly-linked-list/hw-1--1-destructor.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+
+	Node(int data) : data(data)
+	{
+	}
+};
+class LinkedList
+{
+private:
+	Node *head{};
+	Node *tail{};
+
+public:
+	void print()
+	{
+		for (auto ptr = head; ptr; ptr = ptr->next)
+		{
+			cout << ptr->data << " ";
+		}
+		cout << endl;
+	}
+	void insert_end(int x)
+	{
+		if (!head)
+		{
+			head = new Node(x);
+			tail = head;
+			head->next = nullptr;
+			return;
+		}
+		auto new_node = new Node(x);
+		tail->next = new_node;
+		tail = tail->next;
+		tail->next = nullptr;
+	}
+	~LinkedList()
+	{
+		Node *prev = nullptr;
+		for (auto ptr = head; ptr; ptr = ptr->next)
+		{
+			if (prev)
+			{
+				delete prev;
+			}
+			prev = ptr;
+			cout << "destroyed!" << endl;
+		}
+	}
+};
+
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	LinkedList ll;
+	ll.print();
+	ll.insert_end(6);
+	ll.insert_end(10);
+	ll.insert_end(8);
+	ll.insert_end(15);
+	ll.print();
+	return 0;
+}
