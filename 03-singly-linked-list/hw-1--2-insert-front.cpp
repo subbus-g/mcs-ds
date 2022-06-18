7a90db826af0b85ce3de023749672b9b8480f877
subbus <subbareddyg83@gmail.com>
2022-06-18 07:36:36 +0530

    add hw 1 2 insert front

---
+++ 03-singly-linked-list/hw-1--2-insert-front.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+
+	Node(int data) : data(data)
+	{
+	}
+};
+class LinkedList
+{
+private:
+	Node *head{};
+	Node *tail{};
+
+public:
+	void print()
+	{
+		for (auto ptr = head; ptr; ptr = ptr->next)
+		{
+			cout << ptr->data << " ";
+		}
+		cout << endl;
+	}
+	void insert_end(int x)
+	{
+		if (!head)
+		{
+			head = new Node(x);
+			tail = head;
+			head->next = nullptr;
+			return;
+		}
+		auto new_node = new Node(x);
+		tail->next = new_node;
+		tail = tail->next;
+		tail->next = nullptr;
+	}
+	void insert_front(int x)
+	{
+		auto new_node = new Node(x);
+		new_node->next = head;
+		head = new_node;
+	}
+};
+
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	LinkedList ll;
+	ll.insert_end(6);
+	ll.insert_end(10);
+	ll.insert_end(8);
+	ll.insert_end(15);
+	ll.print();
+
+	ll.insert_front(7);
+	ll.insert_front(5);
+	ll.insert_front(1);
+	ll.print();
+
+	return 0;
+}
