66ea822d8623c13b087818f41d68783ea7061332
subbus <subbareddyg83@gmail.com>
2022-06-18 11:33:01 +0530

    add hw 1 6 no tail

---
+++ 03-singly-linked-list/hw-1--6-no-tail.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+
+	Node(int data) : data(data)
+	{
+	}
+};
+class LinkedList
+{
+private:
+	Node *head{};
+
+public:
+	void print()
+	{
+		for (auto ptr = head; ptr; ptr = ptr->next)
+		{
+			cout << ptr->data << " ";
+		}
+		cout << endl;
+	}
+	void add_element(int value)
+	{
+		auto new_node = new Node(value);
+		new_node->next = head;
+		head = new_node;
+	}
+	Node *get_tail()
+	{
+		Node *ptr;
+		//traverse upto the lastnode
+		for (ptr = head; ptr->next; ptr = ptr->next)
+			;
+		return ptr;
+	}
+};
+
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	LinkedList ll;
+	ll.add_element(1);
+	ll.add_element(2);
+	ll.add_element(3);
+	ll.print();
+	cout << "The value at Tail node is: " << ll.get_tail()->data;
+	return 0;
+}
