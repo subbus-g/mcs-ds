d3e2c40df022c452e982bd65efd804abc070f380
subbus <subbareddyg83@gmail.com>
2022-06-19 13:15:36 +0530

    add hw 2 3 reverse list nodes

---
+++ 03-singly-linked-list/hw-2--3-reverse-list-nodes.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+
+	Node(int data) : data(data)
+	{
+	}
+};
+class LinkedList
+{
+private:
+	Node *head{};
+	Node *tail{};
+
+public:
+	void print()
+	{
+		for (auto ptr = head; ptr; ptr = ptr->next)
+		{
+			cout << ptr->data << " ";
+		}
+		cout << endl;
+	}
+	void insert_end(int x) // O(n)-time, O(1)-memory
+	{
+		if (!head)
+		{
+			head = new Node(x);
+			tail = head;
+			head->next = nullptr;
+			return;
+		}
+		auto new_node = new Node(x);
+		tail->next = new_node;
+		tail = tail->next;
+		tail->next = nullptr;
+	}
+	void reverse_list_nodes()
+	{
+		Node *curr, *prev, *backup;
+		for (prev = head, curr = head->next; curr; prev = curr, curr = backup)
+		{
+			backup = curr->next;
+			curr->next = prev;
+		}
+		head->next = nullptr;
+		head = prev;
+	}
+};
+
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	LinkedList ll;
+	ll.print();
+	ll.insert_end(6);
+	ll.insert_end(10);
+	ll.insert_end(8);
+	ll.insert_end(15);
+	ll.print();
+	ll.reverse_list_nodes();
+	ll.print();
+	return 0;
+}
