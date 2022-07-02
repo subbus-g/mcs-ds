27a94829ed9cfb53d3d6485e5dab577e18ee014c
subbus <subbareddyg83@gmail.com>
2022-07-02 19:00:28 +0530

    update hw 4 2 insert alternate

--- 03-singly-linked-list/hw-4--2-insert-alternate.cpp
+++ 03-singly-linked-list/hw-4--2-insert-alternate.cpp
@@ -39,10 +39,12 @@ public:
 		tail = tail->next;
 		tail->next = nullptr;
 	}
+	// O(max(n,m)) time - O(1) memory
 	void insert_alternate(LinkedList &another)
 	{
 		auto ptr1 = head;
 		auto ptr2 = another.head;
+		// traverse both lists until anyone reached the end
 		while (ptr1 && ptr2)
 		{
 			auto old_next = ptr1->next;
@@ -56,12 +58,20 @@ public:
 			ptr1 = old_next;
 			ptr2 = ptr2->next;
 		}
-		for (; ptr2;ptr2=ptr2->next)
+		// add the remaining elements of another if present
+		for (; ptr2; ptr2 = ptr2->next)
 		{
 			auto node_to_insert = new Node(ptr2->data);
+			if (!head)
+			{
+				head = tail = node_to_insert;
+				continue;
+			}
 			tail->next = node_to_insert;
 			tail = node_to_insert;
 		}
+		// integrity check for tail
+		// cout << "tail for below list is: " << (tail?tail->data:0) << endl;
 	}
 };
 
@@ -79,6 +89,8 @@ int main()
 
 	LinkedList another_1;
 	another_1.insert_end(4);
+	another_1.insert_end(5);
+
 	list_1.print();
 	another_1.print();
 	list_1.insert_alternate(another_1);
@@ -90,8 +102,13 @@ int main()
 	list_2.insert_end(1);
 	list_2.insert_end(2);
 	list_2.insert_end(3);
+
+	LinkedList another_2;
+	another_2.insert_end(4);
+
 	list_2.print();
-	// list_2.arrange_odd_even_nodes();
+	another_2.print();
+	list_2.insert_alternate(another_2);
 	list_2.print();
 	cout << "----------------------------" << endl;
 
@@ -100,12 +117,15 @@ int main()
 	list_3.insert_end(1);
 	list_3.insert_end(2);
 	list_3.insert_end(3);
-	list_3.insert_end(4);
-	list_3.insert_end(5);
-	list_3.insert_end(6);
-	list_3.insert_end(7);
+
+	LinkedList another_3;
+	another_3.insert_end(4);
+	another_3.insert_end(5);
+	another_3.insert_end(6);
+
 	list_3.print();
-	// list_3.arrange_odd_even_nodes();
+	another_3.print();
+	list_3.insert_alternate(another_3);
 	list_3.print();
 	cout << "----------------------------" << endl;
 
@@ -126,5 +146,41 @@ int main()
 	list_4.insert_alternate(another_4);
 	list_4.print();
 	cout << "----------------------------" << endl;
+
+	// tc5
+	LinkedList list_5;
+
+	LinkedList another_5;
+	another_5.insert_end(4);
+	another_5.insert_end(5);
+	another_5.insert_end(6);
+	another_5.insert_end(7);
+
+	list_5.print();
+	another_5.print();
+	list_5.insert_alternate(another_5);
+	list_5.print();
+	cout << "----------------------------" << endl;
+
+	// tc6
+	LinkedList list_6;
+	list_6.insert_end(1);
+	list_6.insert_end(2);
+	list_6.insert_end(3);
+
+	LinkedList another_6;
+	list_6.print();
+	another_6.print();
+	list_6.insert_alternate(another_6);
+	list_6.print();
+	cout << "----------------------------" << endl;
+
+	// tc7
+	LinkedList list_7, another_7;
+	list_7.print();
+	list_7.insert_alternate(another_7);
+	list_7.print();
+	cout << "----------------------------" << endl;
+
 	return 0;
 }
