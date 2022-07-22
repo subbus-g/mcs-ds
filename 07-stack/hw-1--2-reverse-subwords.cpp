9fe2cd3e1bcfa709834bdeb184ae6e62fdd22be3
subbus <subbareddyg83@gmail.com>
2022-07-22 12:00:42 +0530

    add hw 1 2 reverse subwords

---
+++ 07-stack/hw-1--2-reverse-subwords.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T, typename Container = std::deque<T>>
+void print_stack(std::stack<T, Container> stack)
+{
+
+	// template <typename T>
+	// void print_stack(std::stack<T> stack){
+
+	// Notice that we're working on a copy here. IMPORTANT
+	std::cout << "stack of elements : [";
+	while (!stack.empty())
+	{
+		T item = stack.top();
+		std::cout << " " << item;
+		stack.pop(); // Poping from a copy doesn't affect the original. We're good here.
+	}
+	std::cout << "]" << std::endl;
+}
+string reverse_subwords(string line)
+{
+	string reversed{};
+	istringstream iss(line);
+	string s;
+
+	while (iss >> s)
+	{
+
+		// cout << s << " ";
+		stack<char> stk;
+		for (auto &c : s)
+		{
+			//	cout << c << " :" << endl;
+			stk.push(c);
+		}
+		while (!stk.empty())
+		{
+			reversed += stk.top();
+			stk.pop();
+		}
+		reversed += " ";
+		//	print_stack(stk);
+	}
+	return reversed;
+}
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	string line;
+	getline(cin, line);
+
+	cout << reverse_subwords(line) << "\n";
+
+	return 0;
+}
