41bd40b2dea0b54844c321eb7a260d42f75fe52b
subbus <subbareddyg83@gmail.com>
2022-07-25 12:01:39 +0530

    add hw 1 4, 1 5

---
+++ 07-stack/hw-1--4-validate-parenthesis-copy.cpp
@@ -0,0 +1,5 @@
+// leetcode link
+// https://leetcode.com/problems/valid-parentheses/
+
+// the solution is in my another repo named 'leetcode'
+//  https://github.com/subbus-g/leetcode/tree/main/0020.%20Valid%20Parentheses
---
+++ 07-stack/hw-1--5-remove-all-adjacent-duplicates.cpp
@@ -0,0 +1,5 @@
+// leetcode link
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+// the solution is in my another repo named 'leetcode'
+//  https://github.com/subbus-g/leetcode/tree/main/1047.%20Remove%20All%20Adjacent%20Duplicates%20In%20String
