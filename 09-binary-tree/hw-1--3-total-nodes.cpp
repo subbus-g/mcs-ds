171cff9037a2d38a09b4599bf0728e34410ff125
subbus <subbareddyg83@gmail.com>
2022-08-15 10:55:23 +0530

    add hw 1 3 total nodes

---
+++ 09-binary-tree/hw-1--3-total-nodes.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+class BinaryTree
+{
+private:
+	int data{};
+	BinaryTree *left{};
+	BinaryTree *right{};
+
+public:
+	BinaryTree(int data) : data(data)
+	{
+	}
+
+	void add(vector<int> values, vector<char> direction)
+	{
+		assert(values.size() == direction.size());
+		BinaryTree *current = this;
+		// iterate on the path, create all necessary nodes
+		for (int i = 0; i < (int)values.size(); ++i)
+		{
+			if (direction[i] == 'L')
+			{
+				if (!current->left)
+					current->left = new BinaryTree(values[i]);
+				else
+					assert(current->left->data == values[i]);
+				current = current->left;
+			}
+			else
+			{
+				if (!current->right)
+					current->right = new BinaryTree(values[i]);
+				else
+					assert(current->right->data == values[i]);
+				current = current->right;
+			}
+		}
+	}
+
+	void print_inorder()
+	{
+		if (left)
+			left->print_inorder();
+		cout << data << " ";
+		if (right)
+			right->print_inorder();
+	}
+	////////////////////////////////////////////
+
+	int nodes_count()
+	{
+		int count = 1;
+		if(left)
+			count += left->nodes_count();
+		if(right)
+			count += right->nodes_count();
+		return count;
+	}
+};
+
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	BinaryTree tree(1);
+	tree.add({2, 4, 7}, {'L', 'L', 'L'});
+	tree.add({2, 4, 8}, {'L', 'L', 'R'});
+	tree.add({2, 5, 9}, {'L', 'R', 'R'});
+	tree.add({3, 6, 15}, {'R', 'R', 'L'});
+
+	tree.print_inorder();
+	cout << endl;
+	cout << "total number of nodes: " << tree.nodes_count() << endl;
+	cout << "expected:" << 10;
+
+	return 0;
+}
